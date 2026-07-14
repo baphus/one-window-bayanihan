3da756eab31df7779e905cf7d6d2e1186b49058b
baphus <sarsonasjosephuskim@gmail.com>
2026-07-14 15:41:45 +0800

    feat: implement mock CloudinaryAvatarService for testing purposes

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,25 @@
 
 namespace Tests;
 
+use App\Services\CloudinaryAvatarService;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Str;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->app->bind(CloudinaryAvatarService::class, fn () => new class extends CloudinaryAvatarService
+        {
+            public function uploadImage(UploadedFile $file, string $folder, string $publicId): string
+            {
+                return 'https://res.cloudinary.com/test/image/upload/'.Str::uuid().'/'.$folder.'/'.$publicId.'.jpg';
+            }
+
+            public function deleteByUrl(?string $url): void {}
+        });
+    }
 }
